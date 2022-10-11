679ea12bbbca45d58519ae87e4a473f80d7e5110
LAPTOP-E8SAGSSA\modani <modani130484@gmail.com>
2022-10-11 23:40:16 +0700

    udcbm

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home 2023</h3>"
+    html = "<h3>Sklearn Prediction</h3>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
